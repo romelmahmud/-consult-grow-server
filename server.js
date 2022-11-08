ddc2bf6207294c0b621a5599c12333f18f5e7f9a
Romel Mahmud <romelmahmud.dev@gmail.com>
2022-11-08 08:32:33 +0600

    mongodb connection complete

--- server.js
+++ server.js
@@ -1,14 +1,61 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 app = express();
 
 const PORT = process.env.PORT || 5000;
+const URI = process.env.MONGO_URI;
 
+app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello from server");
+});
+
+const client = new MongoClient(URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function dbConnect() {
+  try {
+    await client.connect();
+    console.log("Database connected");
+  } catch (error) {
+    console.log(error, error.message);
+  }
+}
+
+dbConnect();
+
+const Services = client.db("service-review").collection("services");
+const Reviews = client.db("service").collection("reviews");
+
+app.post("/services", async (req, res) => {
+  try {
+    const result = await Services.insertOne(req.body);
+
+    if (result.insertedId) {
+      res.send({
+        success: true,
+        message: `Successfully created the ${req.body.name} with id ${result.insertedId}`,
+      });
+    } else {
+      res.send({
+        success: false,
+        error: "Couldn't create the product",
+      });
+    }
+  } catch (error) {
+    console.log(error, error.message);
+    res.send({
+      success: false,
+      error: error.message,
+    });
+  }
 });
 
 app.listen(PORT, () => {
